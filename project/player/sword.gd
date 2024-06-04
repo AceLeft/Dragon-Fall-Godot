d3fb57696b89c92f4016a31c2edad2780f64773f
Wright <brooke.wright@bsu.edu>
2024-06-04 13:43:39 -0400

    Add sword swinging

--- project/player/sword.gd
+++ project/player/sword.gd
@@ -1,10 +1,19 @@
 extends RigidBody2D
 
+var _attacking := false
+
 @onready var _player : Player = $".."
+@onready var _animation_player : AnimationPlayer = $SwordAnimator
 
+func _ready() -> void:
+	_animation_player.play("reset")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta) -> void:
+	if Input.is_action_pressed("attack") and not _attacking:
+		_attacking = true
+		_animation_player.play("swing_sword")
+	
+	#Follow the player
 	if _player.velocity.y != 0:
 		linear_velocity.y = _player.velocity.y - .10
 	else:
@@ -14,3 +23,9 @@ func _process(delta):
 		linear_velocity.x = _player.velocity.x - .10
 	else:
 		linear_velocity.x = _player.velocity.x
+
+
+func _on_sword_animator_animation_finished(anim_name):
+	if anim_name == "swing_sword":
+		_animation_player.play("reset")
+		_attacking = false
