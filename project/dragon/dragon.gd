7ef4aa3e9dab383e6605ee97e06d4b09037a4305
AceLeft <brookerwright2@gmail.com>
2024-06-20 13:48:56 -0400

    Move jaw before and after breathing fire

--- project/dragon/dragon.gd
+++ project/dragon/dragon.gd
@@ -3,17 +3,24 @@ extends Node2D
 var _attacking := false
 
 @onready var _fire = $Fire
-
+@onready var _attack_animator : AnimationPlayer = $AttackAnimations
 
 
 
 func _on_choose_new_attack_timer_timeout() -> void:
 	if not _attacking:
-		_fire.start()
-		$FireTimer.start()
+		_attack_animator.play("lower_jaw")
 		_attacking = true
 
 
 func _on_fire_timer_timeout() -> void:
 	_fire.stop()
-	_attacking = false
+	_attack_animator.play("raise_jaw")
+
+
+func _on_attack_animations_animation_finished(anim_name):
+	if anim_name == "lower_jaw":
+		_fire.start()
+		$FireTimer.start()
+	if anim_name == "raise_jaw":
+		_attacking = false
