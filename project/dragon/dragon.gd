03bc2e1f1d5d2e6cbfc79e59ae2c9d32fe8ee4c2
AceLeft <brookerwright2@gmail.com>
2024-07-09 14:28:00 -0400

    Complete ceiling attack's neck animation

--- project/dragon/dragon.gd
+++ project/dragon/dragon.gd
@@ -2,7 +2,8 @@ extends Node2D
 
 enum ATTACKS{
 	BreatheFire,
-	SlamGround
+	SlamGround,
+	HitCeiling
 }
 
 const _INIT_QUAKE_IMPULSE := Vector2(0, -100)
@@ -12,6 +13,7 @@ const _QUAKE_PIECE_WIDTH := 20
 var _attacking := false
 var _current_quake_impulse := _INIT_QUAKE_IMPULSE
 var _quake_piece_displacement := Vector2(0,0)
+var _attacks_list := []
 
 @onready var _fire = $Fire
 @onready var _attack_animator : AnimationPlayer = $AttackAnimations
@@ -19,16 +21,22 @@ var _quake_piece_displacement := Vector2(0,0)
 @onready var _quake_piece_scene = preload("res://enemies/quake_piece.tscn")
 
 
+func _ready() -> void:
+	_attack_animator.play("RESET")
+	for attack in ATTACKS.values():
+		_attacks_list.append(attack)
+	
 
 func _on_new_attack_timer_timeout() -> void:
 	if not _attacking:
-		var current_attack = ATTACKS.SlamGround
+		var current_attack = _attacks_list.pick_random()
+		_attacking = true
 		if current_attack == ATTACKS.BreatheFire:
 			_attack_animator.play("lower_jaw")
-			_attacking = true
 		elif current_attack == ATTACKS.SlamGround:
-			_attacking = true
 			_attack_animator.play("slam_ground")
+		elif current_attack == ATTACKS.HitCeiling:
+			_attack_animator.play("hit_head")
 
 
 func _on_fire_timer_timeout() -> void:
@@ -44,6 +52,11 @@ func _on_attack_animations_animation_finished(anim_name):
 		_attacking = false
 	if anim_name == "slam_ground":
 		_quake_piece_timer.start()
+	if anim_name == "hit_head":
+		_attack_animator.play("recover_head")
+	if anim_name == "recover_head":
+		_attacking = false
+	
 
 
 func _on_new_quake_piece_timer_timeout() -> void:
